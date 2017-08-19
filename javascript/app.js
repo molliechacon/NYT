06a3abebe2e264b5496c8f95699a72d44054933a
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-08-19 13:33:57 -0600

    added submit to js and minor changes to html

--- javascript/app.js
+++ javascript/app.js
@@ -20,3 +20,21 @@ var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
 
  // console.log(response.response.docs[0].web_url)
+
+	function processSearchSubmit(e) { // nabbed from: https://stackoverflow.com/questions/5384712/capture-a-form-submit-in-javascript
+    if (e.preventDefault) e.preventDefault();    
+
+    // do stuff here
+
+    	return false;
+	}
+
+	var form = document.getElementById('searchForm'); 
+		if (form.attachEvent) {
+    		form.attachEvent("submit", processSearchSubmit);
+		} 
+		else {
+	    	form.addEventListener("submit", processSearchSubmit);
+		}
+
+
